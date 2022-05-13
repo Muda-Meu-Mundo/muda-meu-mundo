4f0327fc0af38adbe69300cb8597d6eff8f74d0c
MarcosABP <prudenciomarcos10@gmail.com>
2022-05-13 20:22:31 -0300

    a

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,15 +11,6 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
 
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'signup',
-    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
-  },
-
 ];
 @NgModule({
   imports: [
