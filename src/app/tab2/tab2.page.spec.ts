587381538bb6711229f070bce58f399fed937a55
MarcosABP <prudenciomarcos10@gmail.com>
2022-05-29 21:48:52 -0300

    mudança router

--- src/app/tab2/tab2.page.spec.ts
+++ src/app/tab2/tab2.page.spec.ts
@@ -1,25 +1,75 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { Tab2Page } from './tab2.page';
 
 describe('Tab2Page', () => {
   let component: Tab2Page;
   let fixture: ComponentFixture<Tab2Page>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [Tab2Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot(), ExploreContainerComponentModule,ReactiveFormsModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab2Page);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    router = TestBed.get(Router)
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should go to frutas page', () => {
+
+    spyOn(router,'navigate')
+
+    component.frutas();
+
+    expect(router.navigate).toHaveBeenCalledWith(['frutas']);
+  });
+
+  it('should go to verduras page', () => {
+
+    spyOn(router,'navigate')
+
+    component.verduras();
+
+    expect(router.navigate).toHaveBeenCalledWith(['verduras']);
+  });
+
+  it('should go to legumes page', () => {
+
+    spyOn(router,'navigate')
+
+    component.legumes();
+
+    expect(router.navigate).toHaveBeenCalledWith(['legumes']);
+  });
+
+  it('should go to graos page', () => {
+
+    spyOn(router,'navigate')
+
+    component.graos();
+
+    expect(router.navigate).toHaveBeenCalledWith(['graos']);
+  });
+
+  it('should go to raizes page', () => {
+
+    spyOn(router,'navigate')
+
+    component.raizes();
+
+    expect(router.navigate).toHaveBeenCalledWith(['raizes']);
   });
 });
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,21 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private router: Router) {}
 
+  frutas(){
+    this.router.navigate(['frutas']);
+  }
+  verduras(){
+    this.router.navigate(['verduras']);
+  }
+  legumes(){
+    this.router.navigate(['legumes']);
+  }
+  graos(){
+    this.router.navigate(['graos']);
+  }
+  raizes(){
+    this.router.navigate(['raizes']);
+  }
 }
