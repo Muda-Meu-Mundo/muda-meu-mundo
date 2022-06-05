fffbd053b056dd9cc134571ac96e2cbfad9264a9
maluqueiroz1 <belo.marialuiza@gmail.com>
2022-06-05 18:58:58 -0300

    registro

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-register',
@@ -8,23 +10,40 @@ import { Router } from '@angular/router';
 })
 export class RegisterPage implements OnInit {
   
-  productList = [
-    {"product" :"Frutas"},
-    {"product" :"Grãos"},
-    {"product" :"Legumes"}, 
-    {"product" :"Raízes"},
-    {"product" :"Verduras" }
-  ]
+
+  public signup: FormGroup;
+  public submitAttempt: boolean = false;  
+
+  constructor(private router: Router,public formBuilder: FormBuilder) {
+    this.signup = formBuilder.group({
+      Photo:[],
+      Name: ['',Validators.compose([Validators.pattern('[a-zA-Z ]*'), Validators.required])],
+      Email: ['',Validators.compose([Validators.required, Validators.email])],
+      Password: ['',[Validators.required]],
+      Address: ['',[Validators.required]],
+      Phone: ['',[Validators.required]],
+      PFrutas: [''],
+      PGraos: [''],
+      PLegumes: [''],
+      PRaizes: [''],
+      PVerduras: ['']
 
 
-  constructor(private router: Router) { }
+  });
+   }
 
   ngOnInit() {
     
   }
 
   register(){
-    this.router.navigate(['login']);
-  }
-
-}
+    this.submitAttempt = true;
+        if(!this.signup.valid){
+            this.signup}
+        else {
+            console.log("success!")
+            console.log(this.signup.value);
+              this.router.navigate(['login'])
+         }
+        }
+      }
